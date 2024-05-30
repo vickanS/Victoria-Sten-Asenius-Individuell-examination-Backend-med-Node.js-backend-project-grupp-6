b997ff42115509329727b631b63d32724e5394ac
Linnea <linnea.sjoholm@live.se>
2024-05-30 15:20:52 +0200

    Created order function

--- routes/coffee.js
+++ routes/coffee.js
@@ -1,18 +1,26 @@
 import { Router } from "express";
 import { menu } from "../config/data.js";
-import { addToCart } from "../services/coffee.js";
+// import { addToCart } from "../services/coffee.js";
+import { createOrder } from "../services/coffee.js";
 
 const router = Router();
 
-// Visar hela menyn
+// "GET" Visar hela menyn
 router.get("/", (req, res) => {
   res.json(menu);
 });
 
+// "POST" Skapa ny order
+router.post("/", createOrder);
+
+// "DELTE" Ta bort order
+
 // POST ny order
-router.post("/order", (req, res) => {
-  addToCart(req.body);
-  res.json({ message: "Order created succssfully" });
-});
+// router.post("/order", (req, res) => {
+//   addToCart(req.body);
+//   res.json({ message: "Order created succssfully" });
+// });
+
+
 
 export default router;
