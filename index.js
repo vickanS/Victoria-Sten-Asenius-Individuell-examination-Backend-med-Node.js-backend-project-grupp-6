aaefca96452c0f6b6b27d09587e0bc6b9b30a94b
vickanS <stennvictoria@gmail.com>
2024-05-30 15:48:10 +0200

    fetched code from other branch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import menuRouter from "./routes/coffee.js";
 import aboutRouter from "./routes/companyInfo.js";
+import orderRouter from "./routes/coffee.js";
 
 const app = express();
 const PORT = 8000;
@@ -11,6 +12,9 @@ app.use(express.json());
 // Routes
 app.use("/menu", menuRouter);
 app.use("/about", aboutRouter);
+
+// post new order
+app.use("/order", orderRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
