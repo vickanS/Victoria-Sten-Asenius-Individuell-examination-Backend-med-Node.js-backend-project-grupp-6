fe45ffe7274d4f9063a0461df8fe5a3e9c0007cb
Linnea <linnea.sjoholm@live.se>
2024-06-01 10:10:57 +0200

    added middlewares

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { createOrder, orderHistory } from "../services/orderService.js"
+import { validateOrder } from "../middleware/validateOrder.js";
 
 const router = Router();
 
 // "POST"/order Funktion för att skapa en ny order
-router.post('/', createOrder);
+router.post('/', validateOrder, createOrder);
 // "GET"/order Få fram orderhistorik
 router.get('/', orderHistory);
 
