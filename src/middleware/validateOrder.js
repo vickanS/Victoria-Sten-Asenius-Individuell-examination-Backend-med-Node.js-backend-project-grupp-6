6cccac1c88da177a53d48c16754c69046e4ef2c6
JofAlm <150859520+JofAlm@users.noreply.github.com>
2024-06-02 20:58:38 +0200

    Add files via upload

--- src/middleware/validateOrder.js
+++ src/middleware/validateOrder.js
@@ -1,15 +1,14 @@
-
 // Middleware för att validera en order
 function validateOrder(req, res, next) {
-    // Destrukturera title och price från req.body
-    const { title, price } = req.body; 
-    // Kontrollera om title eller price saknas
-    if (!title || !price) {
-        // Om någon av dem saknas, skicka en 400-status och ett felmeddelande
-        return res.status(400).json({ error: "Title and price are required" });
-    }
-    // Om båda title och price finns, gå vidare till nästa middleware eller route handler
-    next();
+  // Destrukturera title och price från req.body
+  const { title, price, userId } = req.body;
+  // Kontrollera om title eller price saknas
+  if (!title || !price || !userId) {
+    // Om någon av dem saknas, skicka en 400-status och ett felmeddelande
+    return res.status(400).json({ error: "Title and price are required" });
+  }
+  // Om båda title och price finns, gå vidare till nästa middleware eller route handler
+  next();
 }
 
 export { validateOrder };
