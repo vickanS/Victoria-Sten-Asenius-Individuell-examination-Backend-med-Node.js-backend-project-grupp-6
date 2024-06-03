3dc8cd290a6e8aacb245d12104d686c665d1008b
Patrik Eriksson <puttrik96@hotmail.com>
2024-06-03 15:04:49 +0200

    wip

--- src/routes/order.js
+++ src/routes/order.js
@@ -9,7 +9,7 @@ import { validateOrder } from "../middleware/validateOrder.js";
 
 const router = Router();
 // "POST"/order Funktion för att skapa en ny order
-router.post("/", validateOrder, createOrder);
+router.post("/", createOrder, validateOrder);
 // "GET"/order Få fram kundvagnen med totalpris på ordern
 router.get("/", viewCart);
 // "DELETE"/order ta bort en order
