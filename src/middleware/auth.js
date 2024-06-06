cd05d6faaa27fdf957a3e48aaa41def16e2bee3a
Linnea <linnea.sjoholm@live.se>
2024-06-06 10:34:11 +0200

    code updated

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,19 +3,7 @@
 const authenticate = (req, res, next) => {
   // Kontrollera om det finns en global variabel currentUser som är satt (dvs. användaren är inloggad)
   if (global.currentUser) {
-<<<<<<< HEAD
-  // Om användaren är inloggad, gå vidare till nästa middleware eller route handler
-    next();
-  } else {
-  // Om användaren inte är inloggad, skicka tillbaka ett svar med status 401 (Unauthorized) och ett meddelande
-    res.status(401).json({
-      success: false,
-      message: "You have to be logged in to view your order history",
-      status: 401,
-    });
-=======
     req.user = global.currentUser;
->>>>>>> origin/dev
   }
   next();
 };
--- src/middleware/validateUser.js
+++ src/middleware/validateUser.js
@@ -2,22 +2,6 @@ import joi from "joi";
 
 // Middleware för att validera en användare
 function validateUser(req, res, next) {
-<<<<<<< HEAD
-    // Definiera ett valideringsschema med Joi
-    const userSchema = joi.object({
-    username: joi.string().alphanum().min(3).max(15).required(),
-    password: joi.string().min(5). required(),
-    });
-    // Validera req.body mot schemat
-    const { error } = userSchema.validate(req.body);
-    // Om det finns ett valideringsfel, returnera ett felmeddelande
-    if (error) {
-        return res.status(400).json({ error: error.details[0].message });
-    }
-    // Om valideringen lyckas, gå vidare till nästa middleware eller route handler
-    next();
-};
-=======
   // Definiera ett valideringsschema med Joi
   const userSchema = joi.object({
     username: joi.string().alphanum().min(3).max(15).required(),
@@ -32,6 +16,5 @@ function validateUser(req, res, next) {
   // Om valideringen lyckas, gå vidare till nästa middleware eller route handler
   next();
 }
->>>>>>> origin/dev
 
 export { validateUser };
